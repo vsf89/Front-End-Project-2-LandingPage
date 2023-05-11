8f77203d5a1931953abd8e355b0165d5cdad172e
Vineeta P <vineeta.prashar@gmail.com>
2023-05-10 18:19:55 -0600

    Moved sections to Global vars Added class 'active' to section when near top of viewport

--- Landing Page/js/app.js
+++ Landing Page/js/app.js
@@ -23,8 +23,9 @@
  * 
 */
 const navBarMenu = document.querySelectorAll("navbar__menu");
-// const sections = document.getElementsByTagName('section');
-const div = document.getElementsByClassName('landing__container');
+//const sections = document.querySelectorAll('section');
+const sections = document.getElementsByTagName('section');
+const landingContainer = document.getElementsByClassName('landing__container');
 const sectionNavName = "data-nav";
 
 /**
@@ -34,16 +35,16 @@ const sectionNavName = "data-nav";
 */
 
 
-function createParagraph(paraText){
+function createParagraph(paraText) {
     let para1 = document.createElement("p");
     para1.textContent = paraText;
     return para1;
 }
 
-function addSection(sectionId, sectionName){
+function addSection(sectionId, sectionName) {
     let section = document.createElement("section");
-    section.id = sectionId; 
-    section.setAttribute(sectionNavName, sectionName);  
+    section.id = sectionId;
+    section.setAttribute(sectionNavName, sectionName);
     return section;
 }
 
@@ -51,13 +52,13 @@ function addSection(sectionId, sectionName){
 // ul > li > a
 // ulEl > liEl > aEl
  */
-function createList(ulElement, sections){
+function createList(ulElement, sections) {
     for (let i = 0; i < sections.length; i++) {
         const aElement = document.createElement('a');
         aElement.href = `#section${ulElement.children.length + 1}`;
         aElement.innerHTML += `${aElement}`;
-        aElement.innerText = `${sections[i].getAttribute(sectionNavName)}`;     
-       
+        aElement.innerText = `${sections[i].getAttribute(sectionNavName)}`;
+
         const liElement = document.createElement('li');  // ul > li > a
         liElement.appendChild(aElement);
         ulElement.appendChild(liElement);
@@ -65,24 +66,25 @@ function createList(ulElement, sections){
 }
 
 function createSection(sectionId, sectionName) {
-    let div4 = document.createElement("div");
-    div4.className = 'landing__container';
+    let nextDiv = document.createElement("div");
+    nextDiv.className = 'landing__container';
     let h2 = document.createElement("h2");
     h2.innerText = sectionName;
-    div4.append(h2);
+    nextDiv.append(h2);
 
     let para1 = createParagraph("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.");
 
     let para2 = createParagraph("Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.");
 
-    div4.append(para1);
-    div4.append(para2);
+    nextDiv.append(para1);
+    nextDiv.append(para2);
 
     let section4 = addSection(sectionId, sectionName);
-    section4.appendChild(div4);
+    section4.appendChild(nextDiv);
 
     const mainSection = document.body.getElementsByTagName('main')[0];
     mainSection.appendChild(section4);
+    return mainSection;
 }
 /**
  * End Helper Functions
@@ -92,18 +94,34 @@ function createSection(sectionId, sectionName) {
 
 // build the nav
 
-document.addEventListener('DOMContentLoaded', function () { 
-    createSection('section4', 'Section 4');    
+document.addEventListener('DOMContentLoaded', function () {
+    createSection('section4', 'Section 4');
 
     const ulElement = document.getElementById('navbar__list');
-    const sectionsList = document.getElementsByTagName('section');
-    createList(ulElement, sectionsList);
+    createList(ulElement, sections);
 });
 
 
 // Add class 'active' to section when near top of viewport
 
+window.addEventListener('scroll', update);
 
+function update() {
+    let current = document.getElementsByClassName('active');
+     for (let index = 0; index < sections.length; index++) {
+         const element = sections[index];
+         const rect = element.getBoundingClientRect();
+
+        let scrol = window.scrollY;
+        if (scrol > 0 && rect.top < window.innerHeight && rect.bottom >= window.innerHeight / 2) {                     
+            element.classList.add('active');
+        }
+        else if(current.id !== element.id)
+        {            
+            element.classList.remove('active');
+        }
+     }
+}
 
 // Scroll to anchor ID using scrollTO event
 
